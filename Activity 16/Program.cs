9fd0e1650743c03b1b398519b3a996f733f818c6
Maryam <850005511@nzse.ac.nz>
2025-03-27 19:20:09 +1300

    Add project files.

---
+++ Activity 16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace Task_1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            WriteFriendsToFile();
+        }
+
+        static void WriteFriendsToFile()
+        {
+            string[] friends = { "\nAlice", " \nBob", "\nCharlie", "\nDavid", "\nEve" };
+            File.WriteAllLines("friends.txt", friends);
+            Console.WriteLine("Friends' names written to file.");
+        }
+    }
+
+}
